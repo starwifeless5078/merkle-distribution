887c3c55864de2d9da1cb7ce8b39bf6e92ef7466
starwifeless5078 <69747768+starwifeless5078@users.noreply.github.com>
2022-04-24 16:40:31 +0700

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-etherscan');
+require('@nomiclabs/hardhat-truffle5');
+require('dotenv').config();
+require('hardhat-deploy');
+require('hardhat-gas-reporter');
+require('solidity-coverage');
+
+const networks = require('./hardhat.networks');
+
+module.exports = {
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_KEY,
+    },
+    gasReporter: {
+        enable: true,
+        currency: 'USD',
+    },
+    solidity: {
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000000,
+            },
+        },
+        version: '0.8.9',
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    networks,
+};
